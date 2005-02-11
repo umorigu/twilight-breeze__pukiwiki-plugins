e122444c721616b41fa28b0dfc3ce5d236177ba3
jjyun <hyper.gauntlet@gmail.com>
2005-02-11 10:44:57 +0000

    ・インライン型を付与

--- tracker_summary_item.inc.php/tracker_summary_item.inc.php
+++ tracker_summary_item.inc.php/tracker_summary_item.inc.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: tracker_summary_item.inc.php,v 0.1 2005/01/26 00:55:58 jjyun Exp $
+// $Id: tracker_summary_item.inc.php,v 0.2 2005/01/27 00:55:58 jjyun Exp $
 //
 // License   : GNU General Public License (GPL) 
 // 
@@ -39,8 +39,47 @@ function plugin_tracker_summary_item_convert()
 				list($config,$list) = array_pad(explode('/',$config,2),2,$list);
 		}
 	}
-	return plugin_tracker_summary_item_getsum($page,$refer,$config,$list,$summary_target,
-						  $filter,$cache);
+
+	list( $title, $sum ) = plugin_tracker_summary_item_getsum($page,$refer,$config,$list,$summary_target, $filter,$cache);
+	
+	return $title . 'の合計：' . $sum;
+}
+
+function plugin_tracker_summary_item_inline()
+{
+	global $vars;
+
+	$summary_item = '';
+	$config = 'default';
+	$list = 'list';
+	$page = $refer = $vars['page'];
+	$filter = '';
+	$cache = TRACKER_LIST_CACHE_DEFAULT;
+
+	if (func_num_args())
+	{
+		$args = func_get_args();
+		switch (count($args))
+		{
+		        case 6:
+
+		        case 5:
+			        $cache = is_numeric($args[4]) ? $args[4] : $cache ;
+		        case 4:
+			        $filter = ($args[3] != '') ? $args[3] : NULL ;
+		        case 3:
+			        $summary_target = $args[2];
+			case 2:
+				$args[1] = get_fullname($args[1],$page);
+				$page = is_pagename($args[1]) ? $args[1] : $page;
+			case 1:
+				$config = ($args[0] != '') ? $args[0] : $config;
+				list($config,$list) = array_pad(explode('/',$config,2),2,$list);
+		}
+	}
+	list($title , $sum ) = plugin_tracker_summary_item_getsum($page,$refer,$config,$list,$summary_target, $filter,$cache);
+
+	return $sum;
 }
 
 function plugin_tracker_summary_item_getsum($page, $refer, $config_name, $list,	$summary_item_name,
@@ -79,7 +118,12 @@ function plugin_tracker_summary_item_getsum($page, $refer, $config_name, $list,	
 	{
 		$list->rows = array_filter($list->rows, array($list_filter, 'filters') );
 	}
-	return $list->sum_up($summary_item_name);
+
+	$title = isset( $list->fields[$summary_item_name]->title ) ? 
+	  $list->fields[$summary_item_name]->title : $summary_item_name;
+	$sum  = $list->sum_up($summary_item_name);
+
+	return array( $title , $sum );
 }
 
 
@@ -109,11 +153,11 @@ class Tracker_Summary_Item_sum_up_list extends Tracker_list
 
 			// tracker の ページ内容の取得
 			$this->items = $row;
-			if( array_key_exists($summary_item_name, $this->items) )
+			if( isset($this->items[$summary_item_name]) )
 			{
 				// 値の取得
 				$value = $this->items[$summary_item_name];
-				if( array_key_exists($summary_item_name, $this->fields))
+				if( isset( $this->fields[$summary_item_name]) )
 				{
 					$value = $this->fields[$summary_item_name]->get_value($value);
 				}
