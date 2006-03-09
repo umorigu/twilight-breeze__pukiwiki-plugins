20e15acb6d66c12fdd688c2b5246fd8d3f6e8f36
jjyun <hyper.gauntlet@gmail.com>
2006-03-09 17:33:48 +0000

    modified to update it to v0.9

--- listbox3.inc.php/listbox3.inc.php
+++ listbox3.inc.php/listbox3.inc.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: listbox3.inc.php,v 0.9 2006/03/02 06:20:21 jjyun Exp $
+// $Id: listbox3.inc.php,v 0.9 2006/03/10 01:32:21 jjyun Exp $
 //   This script is based on listbox2.inc.php by KaWaZ
 // -----------------------------------------------------------------
 // Copyright (C)
@@ -18,210 +18,234 @@ define('LISTBOX3_APPLY_FORMAT',TRUE); // TRUE or FALSE
 // モード変更を適用する
 define('LISTBOX3_APPLY_MODECHANGE',TRUE); // TRUE or FALSE
 
-function plugin_listbox3_action() {
-  global $script, $vars;
-  check_editable($vars['refer'], true, true);
+function plugin_listbox3_init()
+{
+	$cfg = array(
+			'_listbox3_cfg' => array (
+				'imgEdit'  => 'paraedit.png',
+				'imgRefer' => 'close.png'
+				)
+			);
+	set_plugin_messages($cfg);
+}
 
-  $number = 0;
-  $pagedata = '';
-  $pagedata_old  = get_source($vars['refer']);
-  foreach($pagedata_old as $line) {
-    if(!preg_match('/^(?:\/\/| )/', $line)) {
-      if (preg_match_all('/(?:#listbox3\(([^\)]*)\))/', $line, $matches, PREG_SET_ORDER)) {
-	$paddata = preg_split('/#listbox3\([^\)]*\)/', $line);
-	$line = $paddata[0];
-	foreach($matches as $i => $match) {
-	  $opt = $match[1];
-	  if($vars['number'] == $number++) {
-	    //ターゲットのプラグイン部分
-	    $opt = preg_replace('/[^,]*/', $vars['value'], $opt, 1);
-	  }
-	  $line .= "#listbox3($opt)" . $paddata[$i+1];
+function plugin_listbox3_action()
+{
+	global $script, $vars;
+	check_editable($vars['refer'], true, true);
+
+	$number = 0;
+	$pagedata = '';
+	$pagedata_old  = get_source($vars['refer']);
+	foreach($pagedata_old as $line)
+	{
+		if( ! preg_match('/^(?:\/\/| )/', $line) &&
+			preg_match_all('/(?:#listbox3\(([^\)]*)\))/',
+						   $line, $matches, PREG_SET_ORDER) )
+		{
+			$paddata = preg_split('/#listbox3\([^\)]*\)/', $line);
+			$line = $paddata[0];
+			foreach($matches as $i => $match)
+			{
+				$opt = $match[1];
+				if($vars['number'] == $number++)
+				{
+					//ターゲットのプラグイン部分
+					$opt = preg_replace('/[^,]*/', $vars['select'], $opt, 1);
+				}
+				$line .= "#listbox3($opt)" . $paddata[$i+1];
+			}
+		}
+		$pagedata .= $line;
 	}
-      }
-    }
-    $pagedata .= $line;
-  }
-  page_write($vars['refer'], $pagedata);
-  if( LISTBOX3_JUMP_TO_MODIFIED_PLACE && $pagedata != '' )
-  {
-    header("Location: $script?".rawurlencode($vars['refer'])."#listbox3_no_".$vars['number']);
-    exit;
-  }
-  return array('msg' => '', 'body' => '');
+	page_write($vars['refer'], $pagedata);
+
+	if( LISTBOX3_JUMP_TO_MODIFIED_PLACE && $pagedata != '' )
+	{
+		header("Location: $script?".rawurlencode($vars['refer'])."#listbox3_no_".$vars['number']);
+		exit;
+	}
+	return array('msg' => '', 'body' => '');
 }
 
 // header宣言の中で以下の２つの定義を行う
 // ・Javasciptを用いること、
 // ・XHTML1.0 Transitional Modeでの動作（<form>タグにname属性を用いる）
-function plugin_listbox3_headDeclaration() {
-  global $pkwk_dtd, $javascript;
+function plugin_listbox3_headDeclaration()
+{
+	global $pkwk_dtd, $javascript;
 
-  // <head> タグ内への <meta>宣言の追加
-  $meta_str =
-   " <meta http-equiv=\"content-script-type\" content=\"text/javascript\" /> ";
-  if(! in_array($meta_str, $head_tags) ){
-    $head_tags[] = $meta_str;
-  }
-
-  // Javasciptを用いること、<form>タグにname属性を用いることを通知する
-  if( PKWK_ALLOW_JAVASCRIPT && LISTBOX3_APPLY_MODECHANGE )
-  {
-    // XHTML 1.0 Transitional
-    if (! isset($pkwk_dtd) || $pkwk_dtd == PKWK_DTD_XHTML_1_1)
-    $pkwk_dtd = PKWK_DTD_XHTML_1_0_TRANSITIONAL;
+	// Javasciptを用いること、<form>タグにname属性を用いることを通知する
+	if( PKWK_ALLOW_JAVASCRIPT && LISTBOX3_APPLY_MODECHANGE )
+	{
+		// XHTML 1.0 Transitional
+		if (! isset($pkwk_dtd) || $pkwk_dtd == PKWK_DTD_XHTML_1_1)
+		{
+			$pkwk_dtd = PKWK_DTD_XHTML_1_0_TRANSITIONAL;
+		}
     
-    // <head> タグ内への <meta>宣言の追加
-    $javascript = TRUE;
-  }
+		// <head> タグ内への <meta>宣言の追加
+		$javascript = TRUE;
+	}
+
+	// <head> タグ内への <meta>宣言の追加
+	$meta_str =
+		" <meta http-equiv=\"content-script-type\" content=\"text/javascript\" /> ";
+	if(! in_array($meta_str, $head_tags) )
+	{
+		$head_tags[] = $meta_str;
+	}
 }
 
 function plugin_listbox3_convert()
 {
-  global $head_tags;
+	global $head_tags;
 
-  $number = plugin_listbox3_getNumber();
+	$number = plugin_listbox3_getNumber();
 
-  // header の宣言
-  if( $number == 0 )
-  {
-    plugin_listbox3_headDeclaration();
-  }
+	// header の宣言
+	if( $number == 0 )
+	{
+		plugin_listbox3_headDeclaration();
+	}
 
-  if(func_num_args() > 1)
-  {
-      $options = func_get_args();
-      $value     = array_shift($options);
-      $template  = array_shift($options);
-      $fieldname = array_shift($options);
-      return plugin_listbox3_getBody($number, $value, $template, $fieldname);
-  }
-  return FALSE;
+	if(func_num_args() > 1)
+	{
+		$options = func_get_args();
+		$value     = array_shift($options);
+		$template  = array_shift($options);
+		$fieldname = array_shift($options);
+		return plugin_listbox3_getBody($number, $value, $template, $fieldname);
+	}
+	return FALSE;
 }
 
-function plugin_listbox3_getNumber() {
-  global $vars;
-  static $numbers = array();
-  if (!array_key_exists($vars['page'],$numbers))
-  {
+function plugin_listbox3_getNumber()
+{
+	global $vars;
+	static $numbers = array();
+	if( !array_key_exists($vars['page'],$numbers) )
+	{
       $numbers[$vars['page']] = 0;
-  }
-  return $numbers[$vars['page']]++;
+	}
+	return $numbers[$vars['page']]++;
 }
 
 function plugin_listbox3_getBody($number, $value, $template, $fieldname)
 {
-  global $script, $vars;
-  $page_enc = htmlspecialchars($vars['page']);
-  $script_enc = htmlspecialchars($script);
-  $body = '';
+	global $script, $vars;
+	global $_listbox3_cfg;
 
-  // listbox3 用の<script>タグの挿入 ( for LISTBOX3_APPLY_MODECHANGE )
-  if( PKWK_ALLOW_JAVASCRIPT && LISTBOX3_APPLY_MODECHANGE )
-  {
-    $body .= ($number == 0) ? plugin_listbox3_getScript() : '';
-  }
+	$page_enc = htmlspecialchars($vars['page']);
+	$script_enc = htmlspecialchars($script);
+	
+	// listbox3 用の<script>タグの挿入 ( for LISTBOX3_APPLY_MODECHANGE )
+	$extrascript = (PKWK_ALLOW_JAVASCRIPT && LISTBOX3_APPLY_MODECHANGE && $number == 0) ? plugin_listbox3_getScript() : '';
 
-  $options_html = plugin_listbox3_getOptions($value, $template, $fieldname);
-  $imagePath = IMAGE_DIR;
-  $editImage = 'paraedit.png';
-  $referImage = 'close.png';
-  $body .= <<<EOD
-    <select name="value" style="vertical-align:middle" onchange="this.form.submit();">
-    $options_html
-    </select>
-    <input type="hidden" name="number" value="$number" />
-    <input type="hidden" name="plugin" value="listbox3" />
-    <input type="hidden" name="refer"  value="$page_enc" />
+	$options_html = plugin_listbox3_getOptions($value, $template, $fieldname);
+	$imgPath  = IMAGE_DIR;
+	$imgEdit  = $_listbox3_cfg[imgEdit];
+	$imgRefer = $_listbox3_cfg[imgRefer];
+	$body = <<<EOD
+	  <select name="select" style="vertical-align:middle" onchange="this.form.submit();">
+	  $options_html
+	  </select>
+	  <input type="hidden" name="number" value="$number" />
+	  <input type="hidden" name="plugin" value="listbox3" />
+	  <input type="hidden" name="refer"  value="$page_enc" />
 EOD;
 
-  if( PKWK_ALLOW_JAVASCRIPT && LISTBOX3_APPLY_MODECHANGE )
-  {
-    $body .= <<< EOD
-    <img name="editTrigger" src="$imagePath$editImage" 
-     onload="listbox3_enableSelector( document.listbox3$number,'$referImage' );"
-    onclick="listbox3_changeEditTriggerImage( document.listbox3$number, '$editImage', '$referImage','$imagePath' );" />
+	if( PKWK_ALLOW_JAVASCRIPT && LISTBOX3_APPLY_MODECHANGE )
+	{
+		$body .= <<< EOD
+		  <img name="editTrigger" src="$imgPath$imgEdit" alt="edit/refer"
+		  onclick="_plugin_listbox3_changeMode( document.listbox3$number, '$imgEdit', '$imgRefer','$imgPath' );" />
 EOD;
-  }
+	}
   
-  if( LISTBOX3_JUMP_TO_MODIFIED_PLACE )
-  {
-    $body = <<< EOD
-    <a id="listbox3_no_$number">
-    $body
-    </a>
+	if( LISTBOX3_JUMP_TO_MODIFIED_PLACE )
+	{
+		$body = <<< EOD
+		  <a id="listbox3_no_$number">
+		  $body
+		  </a>
 EOD;
-  }
-
-  $body = <<< EOD
-  <form name="listbox3$number" action="$script_enc" method="post" style="margin:0;"> 
-  <div>
-  $body
-  </div>
-  </form>
+	}
+	
+	$body = <<< EOD
+	  <form name="listbox3$number" action="$script_enc" method="post" style="margin:0;"> 
+	  <div>
+	  $body
+	  </div>
+	  </form>
 EOD;
-  return $body;
+
+	return $extrascript . $body;
 }
 
-function plugin_listbox3_getScript() {
-  $js = '<script type="text/javascript" src="'. SKIN_DIR . 'listbox3.js" ></script>';
-  return $js;
+function plugin_listbox3_getScript()
+{
+	$js = '<script type="text/javascript" src="'. SKIN_DIR . 'listbox3.js" ></script>';
+	return $js;
 }
 
-function plugin_listbox3_getOptions($value, $config_name, $field_name) {
-  $options_html = '';
+function plugin_listbox3_getOptions($value, $config_name, $field_name)
+{
+	$options_html = '';
   
-  $config = new Config('plugin/tracker/'.$config_name);
-  if(!$config->read()){
-    return "<p>config file '" . 
-      htmlspecialchars($config_name)."' not found.</p>";    
-  }
-  $config->name = $config_name;
+	$config = new Config('plugin/tracker/'.$config_name);
+	if( ! $config->read() )
+	{
+		return "<p>config file '" . htmlspecialchars($config_name)."' not found.</p>";    
+	}
+	$config->name = $config_name;
 
-  $isSelect = 0;
-  foreach($config->get($field_name) as $options) {
-    $s_option=$options[0];
-    $s_format=$options[1];
-    if($s_option == '') continue;
+	$isSelect = 0;
+	foreach($config->get($field_name) as $options)
+	{
+		$s_option=$options[0];
+		$s_format=$options[1];
+		if($s_option == '') continue;
 
-    $option_fmt = ( LISTBOX3_APPLY_FORMAT ) ? plugin_listbox3_getStyle($s_format) : '';
-
-    $option_enc = htmlspecialchars($s_option);
-    if($value == $s_option) {
-      $isSelect = 1;
-      $options_html .= "<option value='$option_enc' style='$option_fmt' selected='selected'>$option_enc</option>";
-    } else {
-      $options_html .= "<option value='$option_enc' style='$option_fmt' >$option_enc</option>";
-    }
-  }
+		$option_fmt = ( LISTBOX3_APPLY_FORMAT ) ? plugin_listbox3_getStyle($s_format) : '';
+		
+		$option_enc = htmlspecialchars($s_option);
+		if($value == $s_option)
+		{
+			$isSelect = 1;
+			$options_html .= "<option value='$option_enc' style='$option_fmt' selected='selected'>$option_enc</option>";
+		}
+		else
+		{
+			$options_html .= "<option value='$option_enc' style='$option_fmt' >$option_enc</option>";
+		}
+	}
   
-  if($isSelect == 0){
-    $options_html =
-      "<option value='…' selected='selected'>…</option>" . $options_html;
-  }
-  return $options_html;
+	if( $isSelect == 0 )
+	{
+		$options_html = "<option value='…' selected='selected'>…</option>" . $options_html;
+	}
+	return $options_html;
 }
 
-function plugin_listbox3_getStyle($s_format) {
+function plugin_listbox3_getStyle($s_format)
+{
+	if( $s_format == '') return '';
   
-  if( $s_format == '')
-    return '';
-  
-  $format_enc = htmlspecialchars($s_format);
-  $format_enc = preg_replace("/\%s/", '', $format_enc);
-
-  $opt_format='';
-  $matches=array();
-  while ( preg_match('/^(?:(BG)?COLOR\(([#\w]+)\)):(.*)$/', $format_enc, $matches))
-  {
-    if ($matches[0])
-      {
-	$style_name = $matches[1] ? 'background-color' : 'color';
-	$opt_format .= $style_name . ':' . htmlspecialchars($matches[2]) . ';';
-	$format_enc = $matches[3];
-      }
-  }
-  return $opt_format;
+	$format_enc = htmlspecialchars($s_format);
+	$format_enc = preg_replace("/\%s/", '', $format_enc);
+	
+	$opt_format='';
+	$matches=array();
+	while ( preg_match('/^(?:(BG)?COLOR\(([#\w]+)\)):(.*)$/', $format_enc, $matches) )
+	{
+		if ($matches[0])
+		{
+			$style_name = $matches[1] ? 'background-color' : 'color';
+			$opt_format .= $style_name . ':' . htmlspecialchars($matches[2]) . ';';
+			$format_enc = $matches[3];
+		}
+	}
+	return $opt_format;
 }
-
 ?>
